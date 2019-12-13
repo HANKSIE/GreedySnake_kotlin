4cb1e3cee98f40ba71e808928128d9ac3f4cf2ce
ShowXD <n16418113@gmail.com>
2019-12-13 23:24:38 +0800

    Modify Help.

--- app/src/main/java/com/example/greedysnake_kotlin/title/Title.kt
+++ app/src/main/java/com/example/greedysnake_kotlin/title/Title.kt
@@ -1,12 +1,23 @@
 package com.example.greedysnake_kotlin.title
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.PopupWindow
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.greedysnake_kotlin.MainActivity
 import com.example.greedysnake_kotlin.R
 import kotlinx.android.synthetic.main.activity_title.*
+
+private const val NUM_PAGES = 4
 
 class Title : AppCompatActivity() {
 
@@ -15,11 +26,17 @@ class Title : AppCompatActivity() {
     private var modeIndex = 0
     private var mode = listOf("normal", "unlimited", "prop")
 
+    private lateinit var hPager: ViewPager
+    private lateinit var hPopupWindow: PopupWindow
+
     // endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
+
+//        hPager = findViewById(R.id.help_pager)
+//        hPager.adapter = HelpPagerAdapter(supportFragmentManager)
 
         // region button
 
@@ -59,13 +76,28 @@ class Title : AppCompatActivity() {
     // 開始
     private fun btnStartSet() {
         Log.d("TAG", mode[modeIndex])
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("mode", mode[modeIndex])
-        startActivity(intent)
+        val startIntent = Intent(this, MainActivity::class.java)
+        startIntent.putExtra("mode", mode[modeIndex])
+        startActivity(startIntent)
     }
 
     // 說明
     private fun btnHelpSet() {
+        hPopupWindow = PopupWindow(hPager, 900, 900)
+        hPopupWindow.isOutsideTouchable = true
+        TransitionManager.beginDelayedTransition(title_layout)
+        hPopupWindow.showAtLocation(title_layout, Gravity.CENTER, 0, 0)
+    }
+
+    private inner class HelpPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment = when (position) {
+            0 -> HelpFragment_1()
+            1 -> HelpFragment_2()
+            2 -> HelpFragment_3()
+            else -> HelpFragment_4()
+        }
+
+        override fun getCount(): Int = NUM_PAGES
 
     }
 
