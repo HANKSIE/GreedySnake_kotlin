eccb54af36d7b39e1990e1c520c5f384bad9ba21
ShowXD <n16418113@gmail.com>
2019-12-13 20:36:36 +0800

    Really complete the title config.

--- app/src/main/java/com/example/greedysnake_kotlin/title/Title.kt
+++ app/src/main/java/com/example/greedysnake_kotlin/title/Title.kt
@@ -1,7 +1,8 @@
-package com.example.greedysnake_kotlin
+package com.example.greedysnake_kotlin.title
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.greedysnake_kotlin.R
 
 class Title : AppCompatActivity() {
 
