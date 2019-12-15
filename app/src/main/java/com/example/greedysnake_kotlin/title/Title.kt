4ac8cac5561677f13d42caa8a0ded776275b9752
ShowXD <n16418113@gmail.com>
2019-12-16 01:42:05 +0800

    Complete the Title Pages Image basic.

--- app/src/main/java/com/example/greedysnake_kotlin/title/Title.kt
+++ app/src/main/java/com/example/greedysnake_kotlin/title/Title.kt
@@ -1,23 +1,12 @@
 package com.example.greedysnake_kotlin.title
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.TransitionManager
 import android.util.Log
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.widget.PopupWindow
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager.widget.ViewPager
 import com.example.greedysnake_kotlin.MainActivity
 import com.example.greedysnake_kotlin.R
 import kotlinx.android.synthetic.main.activity_title.*
-
-private const val NUM_PAGES = 4
 
 class Title : AppCompatActivity() {
 
@@ -26,28 +15,17 @@ class Title : AppCompatActivity() {
     private var modeIndex = 0
     private var mode = listOf("normal", "unlimited", "prop")
 
-    private lateinit var hPager: ViewPager
-    private lateinit var hPopupWindow: PopupWindow
-
     // endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
 
-//        hPager = findViewById(R.id.help_pager)
-//        hPager.adapter = HelpPagerAdapter(supportFragmentManager)
-
         // region button
 
-        // 切換模式左按鈕
-        changeModeL_image.setOnClickListener {
-            modeIndex = modeChange("left", modeIndex)
-        }
-
-        // 切換模式右按鈕
-        changeModeR_image.setOnClickListener {
-            modeIndex = modeChange("right", modeIndex)
+        // 模式按鈕
+        mode_button.setOnClickListener {
+            btnModeSet(modeIndex)
         }
 
         // 開始按鈕
@@ -64,11 +42,17 @@ class Title : AppCompatActivity() {
     }
 
     // 切換模式
-    private fun modeChange(mode: String, modeIndex: Int) : Int {
+    private fun btnModeSet(modeIndex: Int) : Int {
         var index = modeIndex
-        when (mode) {
-            "left" -> index = if (index-1 < 0) { 2 } else { index-1 }
-            "right" -> index = if (index+1 > 2) { 0 } else { index+1 }
+//        index = if (index+1 == 3) { 0 } else { index+1 }
+        index += 1
+        Log.d("TAG", "WTF")
+
+        Log.d("TAG", index.toString())
+        when (index) {
+            0 -> {mode_button.setImageResource(R.drawable.ic_title_mode_limited); mode_background.setImageResource(R.drawable.ic_title_button_white)}
+            1 -> {mode_button.setImageResource(R.drawable.ic_title_mode_unlimited); mode_background.setImageResource(R.drawable.ic_title_button_blue)}
+            2 -> {mode_button.setImageResource(R.drawable.ic_title_mode_props); mode_background.setImageResource(R.drawable.ic_title_button_yellow)}
         }
         return index
     }
@@ -83,22 +67,8 @@ class Title : AppCompatActivity() {
 
     // 說明
     private fun btnHelpSet() {
-        hPopupWindow = PopupWindow(hPager, 900, 900)
-        hPopupWindow.isOutsideTouchable = true
-        TransitionManager.beginDelayedTransition(title_layout)
-        hPopupWindow.showAtLocation(title_layout, Gravity.CENTER, 0, 0)
-    }
-
-    private inner class HelpPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
-        override fun getItem(position: Int): Fragment = when (position) {
-            0 -> HelpFragment_1()
-            1 -> HelpFragment_2()
-            2 -> HelpFragment_3()
-            else -> HelpFragment_4()
-        }
-
-        override fun getCount(): Int = NUM_PAGES
 
     }
+
 
 }
