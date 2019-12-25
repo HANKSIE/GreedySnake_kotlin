a814c5a07d79e1d78fb2f33d37c17a1d69708a96
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2019-12-25 23:31:01 +0800

    防止倒數畫面被跳掉問題

--- app/src/main/java/com/example/greedysnake_kotlin/ClockActivity.kt
+++ app/src/main/java/com/example/greedysnake_kotlin/ClockActivity.kt
@@ -11,17 +11,44 @@ class ClockActivity : AppCompatActivity() {
 
     private var mode = 0
     private lateinit var startIntent: Intent
+    private lateinit var countDown: CountDownTimer
+
+
+
+    companion object{
+        private var nowTime: Long = 4000
+    }
+
+    override fun onStop() {
+        super.onStop()
+        countDown.cancel()
+        countDown = getCountDown(nowTime)
+        Log.e("Msg", "CountDown 畫面被Stop!!!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        countDown.start()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clock)
+        Log.e("Msg", "CountDown 畫面被創建!!!")
 
         test()
 
         // 倒數計時
-        object: CountDownTimer(4000, 1000) {
+        countDown = getCountDown(nowTime)
+        countDown.start()
 
+    }
+
+    private fun getCountDown(nowTime: Long): CountDownTimer{
+        return (object: CountDownTimer(nowTime, 1000) {
             override fun onTick(millisUntilFinished: Long) {
+                ClockActivity.nowTime = millisUntilFinished
+                Log.e("倒數",(millisUntilFinished / 1000).toInt().toString())
                 when ((millisUntilFinished / 1000).toInt()) {
                     3 -> countImageView.setImageResource(R.drawable.ic_count_3)
                     2 -> countImageView.setImageResource(R.drawable.ic_count_2)
@@ -30,9 +57,10 @@ class ClockActivity : AppCompatActivity() {
                 }
             }
             override fun onFinish() {
+                ClockActivity.nowTime = 4000
                 startActivityForResult(startIntent,1)
             }
-        }.start()
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
